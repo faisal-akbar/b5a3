5ff1039224b541ce2d70e3a3d22a5e7bb7fd4d75
Faisal Akbar <faisalcep@yahoo.com>
2025-06-22 00:58:40 -0400

    404_global_error

--- dist/app.js
+++ dist/app.js
@@ -13,4 +13,18 @@ app.use("/api/borrow", borrow_controller_1.borrowRoutes);
 app.get('/', (req, res) => {
     res.send('Welcome to Library Management API with Express, TypeScript & MongoDB');
 });
+// 404 Not Found handler
+app.use((req, res, next) => {
+    res.status(404).json({
+        success: false,
+        message: "Route not found",
+    });
+});
+// Global error handler
+app.use((err, req, res, next) => {
+    res.status(err.status || 500).json({
+        success: false,
+        message: err.message || "Internal Server Error",
+    });
+});
 exports.default = app;
--- src/app.ts
+++ src/app.ts
@@ -1,4 +1,4 @@
-import express, { Application, Request, Response } from 'express';
+import express, { Application, NextFunction, Request, Response } from 'express';
 import { booksRoutes } from './app/controllers/books.controller';
 import { borrowRoutes } from './app/controllers/borrow.controller';
 
@@ -14,6 +14,22 @@ app.use("/api/borrow", borrowRoutes)
 
 app.get('/', (req: Request, res: Response) => {
     res.send('Welcome to Library Management API with Express, TypeScript & MongoDB');
+});
+
+// 404 Not Found handler
+app.use((req: Request, res: Response, next: NextFunction) => {
+  res.status(404).json({
+    success: false,
+    message: "Route not found",
+  });
+});
+
+// Global error handler
+app.use((err: any, req: Request, res: Response, next: NextFunction) => {
+ res.status(err.status || 500).json({
+    success: false,
+    message: err.message || "Internal Server Error",
+  });
 });
 
 
